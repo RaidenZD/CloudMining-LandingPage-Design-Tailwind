493fb15963e392a3977661adaa0964bc82ee37c8
RaidenZD <98083248+RaidenZD@users.noreply.github.com>
2023-04-06 14:03:03 +0330

    final push

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
